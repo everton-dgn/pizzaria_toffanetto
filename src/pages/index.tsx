596bbf483a4d438915ff4819622bbbdb82532ab4
everton-dgn <evertondgn@hotmail.com>
2021-04-05 22:04:32 -0300

    update api json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const Home = ({ dataApi }: InferGetStaticPropsType<typeof getStaticProps>) => {
 }
 
 export const getStaticProps: GetStaticProps = async context => {
-  const res = await axios.get('https://querocriarsite.com/api/pizzas.json')
+  const res = await axios.get('http://localhost:3000/api/pizzas.json')
 
   const dataApi = res.data[0]
 
