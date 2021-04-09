36d915ab0a9c086e4a4fb126525efc80a69ae584
everton-dgn <evertondgn@hotmail.com>
2021-04-09 08:57:37 -0300

    correção de bug no select

--- src/components/Forms/FormData/index.tsx
+++ src/components/Forms/FormData/index.tsx
@@ -24,6 +24,36 @@ interface FormDataUnform {
   }
 }
 
+const statesOptions = [
+  { value: 'AC', label: 'AC' },
+  { value: 'AL', label: 'AL' },
+  { value: 'AP', label: 'AP' },
+  { value: 'AM', label: 'AM' },
+  { value: 'BA', label: 'BA' },
+  { value: 'CE', label: 'CE' },
+  { value: 'DF', label: 'DF' },
+  { value: 'ES', label: 'ES' },
+  { value: 'GO', label: 'GO' },
+  { value: 'MA', label: 'MA' },
+  { value: 'MT', label: 'MT' },
+  { value: 'MS', label: 'MS' },
+  { value: 'MG', label: 'MG' },
+  { value: 'PA', label: 'PA' },
+  { value: 'PB', label: 'PB' },
+  { value: 'PR', label: 'PR' },
+  { value: 'PE', label: 'PE' },
+  { value: 'PI', label: 'PI' },
+  { value: 'RJ', label: 'RJ' },
+  { value: 'RN', label: 'RN' },
+  { value: 'RS', label: 'RS' },
+  { value: 'RO', label: 'RO' },
+  { value: 'RR', label: 'RR' },
+  { value: 'SC', label: 'SC' },
+  { value: 'SP', label: 'SP' },
+  { value: 'SE', label: 'SE' },
+  { value: 'TO', label: 'TO' }
+]
+
 export const FormData = () => {
   const { setFormData } = useContext(DataContext)
   const formRef = useRef<FormHandles>(null)
@@ -188,6 +218,7 @@ export const FormData = () => {
                   id="state"
                   label="Estado"
                   placeholder="Estado"
+                  options={statesOptions}
                   setStateInSelect={selectState}
                 />
               </Scope>
--- src/components/Forms/Select/index.tsx
+++ src/components/Forms/Select/index.tsx
@@ -16,36 +16,6 @@ interface Props extends SelectProps<OptionTypeBase> {
   setStateInSelect?: any
 }
 
-const statesOptions = [
-  { value: 'AC', label: 'AC' },
-  { value: 'AL', label: 'AL' },
-  { value: 'AP', label: 'AP' },
-  { value: 'AM', label: 'AM' },
-  { value: 'BA', label: 'BA' },
-  { value: 'CE', label: 'CE' },
-  { value: 'DF', label: 'DF' },
-  { value: 'ES', label: 'ES' },
-  { value: 'GO', label: 'GO' },
-  { value: 'MA', label: 'MA' },
-  { value: 'MT', label: 'MT' },
-  { value: 'MS', label: 'MS' },
-  { value: 'MG', label: 'MG' },
-  { value: 'PA', label: 'PA' },
-  { value: 'PB', label: 'PB' },
-  { value: 'PR', label: 'PR' },
-  { value: 'PE', label: 'PE' },
-  { value: 'PI', label: 'PI' },
-  { value: 'RJ', label: 'RJ' },
-  { value: 'RN', label: 'RN' },
-  { value: 'RS', label: 'RS' },
-  { value: 'RO', label: 'RO' },
-  { value: 'RR', label: 'RR' },
-  { value: 'SC', label: 'SC' },
-  { value: 'SP', label: 'SP' },
-  { value: 'SE', label: 'SE' },
-  { value: 'TO', label: 'TO' }
-]
-
 export const Select = ({
   name,
   id,
@@ -64,17 +34,18 @@ export const Select = ({
       ref: selectRef.current,
       path: 'state.value.value'
     })
+  }, [fieldName, registerField, renderAndSaveSelected])
+
+  useEffect(() => {
     if (setStateInSelect) {
       // @ts-ignore
       changeFocusAdd(selectRef.current?.select.inputRef)
       // @ts-ignore
       changeFocusRemove(selectRef.current?.select.inputRef)
       // @ts-ignore
-      // selectRef.current?.select.setValue(setStateInSelect)
+      selectRef.current?.select.setValue(setStateInSelect)
     }
-    // @ts-ignore
-    console.log(selectRef.current?.state.value)
-  }, [fieldName, registerField, renderAndSaveSelected, setStateInSelect])
+  }, [setStateInSelect])
 
   const clearField = () => {
     if (error !== undefined) clearError()
@@ -91,8 +62,6 @@ export const Select = ({
   }
 
   const changeFocusRemove = (e: any) => {
-    // @ts-ignore
-    selectRef.current?.select.clearValue()
     e.parentNode.parentNode.parentNode.parentNode.parentNode.classList.remove(
       'focusSelect'
     )
@@ -120,7 +89,6 @@ export const Select = ({
               id={id}
               ref={selectRef}
               {...rest}
-              options={statesOptions}
               placeholder={placeholder}
               noOptionsMessage={() => 'Nenhum resultado!'}
               isClearable
