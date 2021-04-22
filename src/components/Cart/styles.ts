ad69c6130123e9e2d2f0f233c205823edb3499d2
everton-dgn <evertondgn@hotmail.com>
2021-04-22 01:27:00 -0300

    ajuste de cores no carrinho

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ContainerCart = styled.div`
   margin-left: auto;
-  color: #34d399;
+  color: #222;
   margin-bottom: 0.2rem;
   display: flex;
 
@@ -20,7 +20,7 @@ export const Cart = styled.h1`
   text-align: right;
   font-size: 2.2rem;
   font-weight: 600;
-  color: #34d399;
+  color: #7f1d1d;
   border: 0.2rem solid #34d399;
   border-radius: 1rem;
   padding: 1rem;
@@ -28,7 +28,7 @@ export const Cart = styled.h1`
   margin: 0 0 1.6rem 1rem;
 
   b {
-    color: #7f1d1d;
+    color: #222;
     font-weight: 600;
     margin-left: 0.5rem;
   }
