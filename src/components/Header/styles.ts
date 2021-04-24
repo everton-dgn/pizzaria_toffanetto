b63828f131e3d59b68acbdbd9d33f10c8dbc88fc
everton-dgn <evertondgn@hotmail.com>
2021-04-24 19:15:19 -0300

    animation css do logo simplificada

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -34,7 +34,7 @@ export const Logo = styled.span`
     padding: 0 2rem !important;
     object-fit: contain;
     opacity: 0;
-    animation: ${s.fadeIn} 0.4s ease-in, ${s.fadeIn} 0.3s linear forwards;
+    animation: ${s.moveUp} 0.4s ease-in forwards;
     animation-delay: 300ms;
   }
 `
