ee6f177f850999796f78235ddf9f6b35985263ef
everton-dgn <evertondgn@hotmail.com>
2021-05-06 19:55:34 -0300

    update apple

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,20 @@ import * as C from 'components'
 import NProgress from 'nprogress'
 import Router from 'next/router'
 import 'styles/loading.css'
+import { useEffect } from 'react'
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    if (navigator.userAgent.match(/Mac|iPad|iPod|iPhone/)) {
+      // @ts-ignore
+      document.querySelector('html').style.fontSize = '40%!important'
+    }
+  }, [])
+
   return (
     <>
       <DataStorage>
