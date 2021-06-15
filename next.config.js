ec1a915aeed06bee38f25ad3d926970e1d404c2d
everton-dgn <evertondgn@hotmail.com>
2021-06-15 14:24:59 -0300

    upgrade next 11.0.0

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
-  future: { webpack5: true },
   pwa: {
     disable: !isProd,
     dest: 'public'
