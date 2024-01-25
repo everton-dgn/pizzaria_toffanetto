188e25c6ef83f5961badff483a0e8d891b393591
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-25 00:51:44 -0300

    fix: fix tests

--- src/tests/pages/home.spec.ts
+++ src/tests/pages/home.spec.ts
@@ -4,23 +4,6 @@ test.describe('navigation', () => {
   test.beforeEach(async ({ page }) => await page.goto('/'))
 
   test('has title', async ({ page }) => {
-    await expect(page).toHaveTitle(/Home Page/)
-  })
-
-  test('has header', async ({ page }) => {
-    const header = page.locator('header[role="banner"]')
-    await expect(header).toHaveText('Boilerplate')
-  })
-
-  test('must interact with the CounterButton', async ({ page }) => {
-    await page.click('button[aria-label="COUNT: 0"]')
-
-    const buttonText = await page.innerText('button[aria-label="COUNT: 1"]')
-    expect(buttonText).toBe('COUNT: 1')
-
-    await page.getByRole('button', { name: 'COUNT: 1' }).click()
-
-    const buttonText2 = page.getByRole('button', { name: 'COUNT: 2' })
-    await expect(buttonText2).toBeVisible()
+    await expect(page).toHaveTitle('Página Inicial')
   })
 })
