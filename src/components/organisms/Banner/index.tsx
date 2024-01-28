6a47ed3e30584b5243f3862ece59311f464e4f62
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-28 03:44:05 -0300

    feat(components): improvement layout and responsiveness of banner component

--- src/components/organisms/Banner/index.tsx
+++ src/components/organisms/Banner/index.tsx
@@ -12,21 +12,21 @@ export const Banner = () => {
 
   return (
     <>
-      <div className={S.containerBanner}>
-        <div className={S.containerInfo}>
+      <div className={S.container}>
+        <div className={S.wrapper_info}>
           <img
             src="/img/logos/logo-vertical.png"
             alt="Pizzaria Toffanetto"
             width="885"
             height="410"
+            className={S.logo}
           />
-          <h3>
+          <h1 className={S.description}>
             Pizzas exageradamente recheadas e saborosas, feitas em forno a lenha
             como manda a tradição italiana
-          </h3>
+          </h1>
         </div>
-
-        <div className={S.containImg}>
+        <div className={S.wrapper_image}>
           {hasNetwork ? (
             <Image
               src="/img/banners/banner-home.webp"
