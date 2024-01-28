1da25bb29640a49d1ffa26ea147e5b661f52ff55
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-28 03:22:06 -0300

    feat(app): add list of cards and categories in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,32 +1,32 @@
 import { notFound } from 'next/navigation'
 
-import { ContainerCardControlRender, TitleSection } from 'components/atoms'
-import { CardsSquare } from 'components/molecules'
+import { CardProduct } from 'components/molecules'
 import { Banner } from 'components/organisms'
 
-import { dailyPizzaOptionsFindAll } from 'infra/services/pizzas/findAll'
+import { availableProductsFindAll } from 'infra/services/availableProducts/findAll'
 
 import S from './styles.module.scss'
 
 const Home = async () => {
-  const { data: pizzas } = await dailyPizzaOptionsFindAll()
+  const { data: availableProducts } = await availableProductsFindAll()
 
-  if (!pizzas) notFound()
+  if (!availableProducts) notFound()
 
   return (
     <>
       <Banner />
-      <TitleSection title="Peça a sua pizza online agora!" />
-      <div className={S.container}>
-        <p className={S.paragraph}>
-          Aproveite a nossa promoção! Comprando a nossa recomendação de pizza do
-          dia você acumula pontos para a próxima compra. Quantos mais pontos
-          você acumular, maior será o desconto na próxima compra.
-        </p>
-        <ContainerCardControlRender minHeight={678}>
-          <CardsSquare data={pizzas} />
-        </ContainerCardControlRender>
-      </div>
+      <main className={S.container}>
+        {availableProducts.map(({ id, category, items }) => (
+          <section key={id} className={S.section}>
+            <h2 className={S.title}>{category}</h2>
+            <div className={S.products}>
+              {items.map(item => (
+                <CardProduct key={item.id} category={category} {...item} />
+              ))}
+            </div>
+          </section>
+        ))}
+      </main>
     </>
   )
 }
