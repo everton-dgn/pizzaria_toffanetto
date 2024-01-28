dcebb30b15f7ac495f1563e2b0a4a8f2d055447c
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-28 03:10:16 -0300

    fix(constants): fix alias image

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -5,7 +5,7 @@ export const NAME_APP = 'Pizzaria Toffanetto'
 export const DESCRIPTION =
   'Este projeto tem o objetivo de agilizar o processo do pedido de pizzas, proporcionando uma experiência melhor para o cliente.'
 
-export const IMAGE_OG = '/img/logo-og.jpg'
+export const IMAGE_OG = '/img/logos/logo-og.jpg'
 
 export const ICON_512_X_512 = '/icons/icon-512x512.png'
 
