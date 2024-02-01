be3b708d364588f2d1ad0db021362d57094147c3
Éverton Toffanetto <evertondgn@hotmail.com>
2024-02-01 07:47:10 -0300

    chore: improvement nextjs config

--- next.config.js
+++ next.config.js
@@ -16,30 +16,9 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   }
 })
 
-const headers = [
-  {
-    source: '/images/:path*',
-    headers: [
-      {
-        key: 'Cache-Control',
-        value: 'public, max-age=31536000, immutable'
-      }
-    ]
-  },
-  {
-    source: '/:path*',
-    headers: [
-      {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=63072000; includeSubDomains; preload'
-      }
-    ]
-  }
-]
-
 const nextConfig = {
-  headers: async () => headers,
   images: {
+    deviceSizes: [450, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [{
       protocol: 'http',
       hostname: 'localhost'
@@ -49,7 +28,6 @@ const nextConfig = {
     path: '/_next/image'
   },
   reactStrictMode: true,
-  productionBrowserSourceMaps: true,
   poweredByHeader: false,
 }
 
