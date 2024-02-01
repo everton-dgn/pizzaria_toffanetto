b7de97d73cba4dc8b3d0298e4e59c59f94d83e8e
Éverton Toffanetto <evertondgn@hotmail.com>
2024-02-01 07:58:34 -0300

    feat(components): export components

--- src/components/organisms/OrderCreationModal/components/index.tsx
+++ src/components/organisms/OrderCreationModal/components/index.tsx
@@ -1,2 +1,4 @@
 export { HeaderProduct } from './HeaderProduct'
 export { SectionProduct } from './SectionProduct'
+export { SizeRadioButtonGroup } from './SizeRadioButtonGroup'
+export { FlavorQuantitySelectorGroup } from './FlavorQuantitySelectorGroup'
