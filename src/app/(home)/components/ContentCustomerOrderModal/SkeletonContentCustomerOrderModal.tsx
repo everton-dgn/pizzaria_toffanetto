6c46e5cad3c6474947042d11025c8c62c5f46f3e
Éverton Toffanetto <evertondgn@hotmail.com>
2024-02-16 01:02:33 -0300

    refactor(app): simplify styles

--- src/app/(home)/components/ContentCustomerOrderModal/SkeletonContentCustomerOrderModal.tsx
+++ src/app/(home)/components/ContentCustomerOrderModal/SkeletonContentCustomerOrderModal.tsx
@@ -2,64 +2,21 @@ import { Skeleton } from 'components/atoms'
 
 const SkeletonSection = () => (
   <>
-    <Skeleton width="100%" height="70px" />
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        gap: '8px',
-        padding: '8px 24px 24px 24px'
-      }}
-    >
-      {Array.from({ length: 4 }, (_, i) => (
-        <Skeleton
-          key={i}
-          height="16px"
-          borderRadius="4px"
-          margin={[i === 0 ? 8 : 4, 0, 4, 0]}
-        />
-      ))}
+    <Skeleton height="70px" />
+    <div className="col-full gap-16 px-24 pt-24 pb-24">
+      <Skeleton variant="text" count={4} />
     </div>
   </>
 )
 
 export const SkeletonContentCustomerOrderModal = () => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      width: '100%'
-    }}
-  >
-    <Skeleton height="auto" aspectRatio="600/317" margin={[0, 0, 12, 0]} />
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        gap: '8px',
-        padding: '0 24px 24px 24px'
-      }}
-    >
-      <Skeleton
-        width="174px"
-        height="24px"
-        margin={[12, 0, 12, 0]}
-        borderRadius="4px"
-      />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '100%',
-          gap: '8px',
-          height: '100%'
-        }}
-      >
-        <Skeleton height="16px" borderRadius="4px" />
-        <Skeleton height="16px" borderRadius="4px" />
-        <Skeleton width="100px" height="16px" borderRadius="4px" />
+  <div className="col-full">
+    <Skeleton className="mb-12 h-auto" aspectRatio="600/317" />
+    <div className="col-full gap-8 px-24 pb-24">
+      <Skeleton variant="text" className="my-12" width="174px" height="24px" />
+      <div className="col-full gap-8 h-100">
+        <Skeleton variant="text" count={2} />
+        <Skeleton variant="text" width="100px" />
       </div>
     </div>
     <SkeletonSection />
