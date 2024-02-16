c4ec9c3200a76ee878481d60368a06ae608ac5cd
Éverton Toffanetto <evertondgn@hotmail.com>
2024-02-16 01:01:00 -0300

    refactor(app): simplify styles

--- src/app/(home)/components/FooterCustomerOrderModal/Skeleton.tsx
+++ src/app/(home)/components/FooterCustomerOrderModal/Skeleton.tsx
@@ -4,7 +4,7 @@ import S from './styles.module.scss'
 
 export const SkeletonFooterCustomerOrderModal = () => (
   <div className={S.footer_modal}>
-    <Skeleton width="90px" height="32px" borderRadius="32px" />
-    <Skeleton width="180px" height="32px" borderRadius="32px" />
+    <Skeleton width="90px" height="32px" className="rounded-32" />
+    <Skeleton width="180px" height="32px" className="rounded-32" />
   </div>
 )
