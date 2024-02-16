16990ee764f7aab5eaff8eefeb9a55ca3aba92ba
Éverton Toffanetto <evertondgn@hotmail.com>
2024-02-15 23:30:27 -0300

    feat(components): improvement style skeleton

--- src/components/atoms/Skeleton/index.tsx
+++ src/components/atoms/Skeleton/index.tsx
@@ -5,32 +5,34 @@ import S from './styles.module.scss'
 import type { SkeletonProps } from './types'
 
 export const Skeleton = ({
-  className,
+  variant,
   height,
-  margin,
   width,
-  borderRadius,
-  aspectRatio
+  aspectRatio,
+  count = 1,
+  className
 }: SkeletonProps) => {
-  const calcMargin = margin
-    ? margin
-        .map(value => {
-          if (typeof value === 'string') return value
-          return `${value}px`
-        })
-        .join(' ')
-    : 0
-
   return (
-    <div
-      className={clsx(S.container, className)}
-      style={{
-        borderRadius: borderRadius || 0,
-        height: height || '100%',
-        width: width || '100%',
-        margin: calcMargin,
-        aspectRatio: aspectRatio || 'auto'
-      }}
-    />
+    <>
+      {Array.from({ length: count }, (_, i) => (
+        <div
+          key={i}
+          className={clsx(
+            S.container,
+            variant === 'text' && 'rounded-4 h-16',
+            variant === 'circle' && 'rounded-circle aspect-square',
+            variant === 'square' && 'aspect-square',
+            className
+          )}
+          style={{
+            height: height || '100%',
+            width: width || '100%',
+            ...(variant === 'text'
+              ? {}
+              : { aspectRatio: aspectRatio || 'auto' })
+          }}
+        />
+      ))}
+    </>
   )
 }
--- src/components/atoms/Skeleton/types.ts
+++ src/components/atoms/Skeleton/types.ts
@@ -1,10 +1,8 @@
-type StringOrNumber = string | number
-
 export interface SkeletonProps {
-  borderRadius?: string
+  variant?: 'square' | 'circle' | 'text'
   className?: string
   height?: string
-  margin?: [StringOrNumber, StringOrNumber, StringOrNumber, StringOrNumber]
   width?: string
   aspectRatio?: string
+  count?: number
 }
