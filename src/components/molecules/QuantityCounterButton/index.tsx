ee61eecaefcf3ec605ae7e27cc53734fb41b7858
Éverton Toffanetto <evertondgn@hotmail.com>
2024-03-11 03:16:27 -0300

    refactor: improvement theme

--- src/components/molecules/QuantityCounterButton/index.tsx
+++ src/components/molecules/QuantityCounterButton/index.tsx
@@ -3,6 +3,9 @@ import { FaPlus as IconPlus, FaMinus as IconMinus } from 'react-icons/fa6'
 
 import type { QuantityCounterButtonProps } from './types'
 
+const btn =
+  'hover:bg-grey-light-100 flex size-[30px] min-h-[30px] min-w-[30px] cursor-pointer rounded-circle bg-soft-white-blue transition-colors duration-150 ease-linear center active:bg-soft-white-blue disabled:cursor-not-allowed disabled:hover:bg-light-blue-hint disabled:active:bg-light-blue-hint'
+
 export const QuantityCounterButton = ({
   quantity,
   onIncrease,
@@ -25,7 +28,7 @@ export const QuantityCounterButton = ({
           <button
             onClick={onDecrease}
             aria-label="Diminuir quantidade em 1"
-            className="hover:bg-grey-light-blue-hint flex size-[30px] min-h-[30px] min-w-[30px] cursor-pointer rounded-circle bg-light-blue-hint transition-colors duration-200 ease-linear center active:bg-light-blue-hint disabled:cursor-not-allowed disabled:hover:bg-light-blue-hint disabled:active:bg-light-blue-hint"
+            className={btn}
           >
             <IconMinus className="size-12 min-h-12 min-w-12 fill-primary-500" />
           </button>
@@ -37,7 +40,7 @@ export const QuantityCounterButton = ({
       <button
         onClick={onIncrease}
         aria-label="Aumentar quantidade em 1"
-        className="hover:bg-grey-light-blue-hint flex size-[30px] min-h-[30px] min-w-[30px] cursor-pointer rounded-circle bg-light-blue-hint transition-colors duration-200 ease-linear center active:bg-light-blue-hint disabled:cursor-not-allowed disabled:hover:bg-light-blue-hint disabled:active:bg-light-blue-hint"
+        className={btn}
       >
         <IconPlus className="size-12 min-h-12 min-w-12 fill-primary-500" />
       </button>
