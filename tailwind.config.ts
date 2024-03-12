d29554cd052ca30f6e19700e4d6eedbd57c7b669
Éverton Toffanetto <evertondgn@hotmail.com>
2024-03-12 09:53:05 -0300

    feat: update theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,13 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 import type { Config } from 'tailwindcss'
 
-import containerQueries from '@tailwindcss/container-queries'
-
 import {
   colors,
   zIndex,
-  spacing,
-  fontWeight,
-  fontSize,
-  boxShadow,
+  spacings,
+  fontWeights,
+  fontSizes,
+  boxShadows,
   borderRadius,
   screens,
   keyframes
@@ -17,24 +15,25 @@ import {
 import { utilities } from './src/theme/utilities'
 import { components } from './src/theme/components'
 
-export default {
+const config: Config = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    screens,
+    colors,
+    zIndex,
+    borderRadius,
+    fontWeight: fontWeights,
+    fontSize: fontSizes,
     extend: {
-      width: { ...defaultTheme.width, ...spacing },
-      height: { ...defaultTheme.height, ...spacing },
-      screens,
-      spacing: { ...defaultTheme.spacing, ...spacing },
-      colors,
-      borderRadius: { ...defaultTheme.borderRadius, ...borderRadius },
-      borderWidth: { ...defaultTheme.borderWidth, ...spacing },
-      zIndex: { ...defaultTheme.zIndex, ...zIndex },
-      fontWeight: { ...defaultTheme.fontWeight, ...fontWeight },
-      boxShadow: { ...defaultTheme.boxShadow, ...boxShadow },
-      fontSize,
+      width: { ...defaultTheme.width, ...spacings },
+      height: { ...defaultTheme.height, ...spacings },
+      spacing: { ...defaultTheme.spacing, ...spacings },
+      borderWidth: { ...defaultTheme.borderWidth, ...spacings },
+      boxShadow: { ...defaultTheme.boxShadow, ...boxShadows },
+      keyframes: { ...defaultTheme.keyframes, ...keyframes },
       fontFamily: {
         sans: 'var(--font-quicksand)'
       },
@@ -42,9 +41,10 @@ export default {
         ...defaultTheme.aspectRatio,
         '600/317': '600/317',
         '10/7': '10/7'
-      },
-      keyframes: { ...defaultTheme.keyframes, ...keyframes }
+      }
     }
   },
-  plugins: [containerQueries, ...components, ...utilities]
-} satisfies Config
+  plugins: [...components, ...utilities]
+}
+
+export default config
